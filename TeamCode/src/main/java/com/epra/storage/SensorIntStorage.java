e6dcbd5793bd47d5e3826de873af40c50321078a
mallratsftc01 <mallratsftc01@gmail.com>
2023-12-16 13:39:33 -0500

    12/16B/2023
    -Renamed certain methods of SensorIntStorage.java to better reflect their purpose

--- TeamCode/src/main/java/com/epra/storage/SensorIntStorage.java
+++ TeamCode/src/main/java/com/epra/storage/SensorIntStorage.java
@@ -1,28 +1,28 @@
 package com.epra.storage;
 
 public class SensorIntStorage {
-    private int[] encoderValues = new int[0];
+    private int[] sensorValues = new int[0];
     /**Stores values from sensors in integer form.
      *<p></p>
      *Queer Coded by Zachy K. If you use this class or a method from this class in its entirety, please make sure to give credit.
      *<p></p>
      *Intended to store values from motor encoders. */
-    public SensorIntStorage(int[] startValues) {setEncoderValues(startValues);}
+    public SensorIntStorage(int[] startValues) {setSensorValues(startValues);}
 
     /**Sets the stored values to match an array.*/
-    public void setEncoderValues(int[] setValues) {
-        if (encoderValues.length != setValues.length) {encoderValues = new int[setValues.length];}
-        for (int ii = 0; ii < setValues.length; ii++) {encoderValues[ii] = setValues[ii];}
+    public void setSensorValues(int[] setValues) {
+        if (sensorValues.length != setValues.length) {sensorValues = new int[setValues.length];}
+        for (int ii = 0; ii < setValues.length; ii++) {sensorValues[ii] = setValues[ii];}
     }
     /**Sets the value of a certain int in the stored array. If provided index is out of range, will return false.*/
-    public boolean setEncoderValue(int value, int index) {
-        if (index < encoderValues.length) {encoderValues[index] = value;
+    public boolean setSensorValue(int value, int index) {
+        if (index < sensorValues.length) {sensorValues[index] = value;
     return true;} else {return false;}
     }
     /**Returns the stored int array.*/
-    public int[] getEncoderValues() {return encoderValues;}
+    public int[] getSensorValues() {return sensorValues;}
     /**Returns a certain value from the stored array if successful. If provided index is out of range, will return false.*/
-    public int getEncoderValue(int index) {if (index < encoderValues.length) {return encoderValues[index];}
+    public int getSensorValue(int index) {if (index < sensorValues.length) {return sensorValues[index];}
         else {return 0;}
     }
 }
